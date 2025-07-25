13ef167dce40e8e674d4160cc0e132e028ecee3d
deltron-fr <kayceoogbonnaya2304@gmail.com>
2025-07-25 13:22:15 +0100

    removed the infra repo

--- infrastructure/terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraformstate"
-    storage_account_name = "terraformstate001"
-    container_name       = "terraformcontainer"
-    key                  = "terraform.tfstate"
-  }
-}
--- infrastructure/terraform/ouptut.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip" {
-  value = azurerm_linux_virtual_machine.main.public_ip_address
-}
